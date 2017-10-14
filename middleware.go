0c2281e44a1f72bfa3889f44d1706228f29fab77
Saravanan Silvarajoo <saravanan.silvarajoo@accedo.tv>
2017-10-14 23:13:07 +0800

    added more functions

--- middleware.go
+++ middleware.go
@@ -2,24 +2,24 @@ package middleware
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
 // ConfigType is a required to start server
 type ConfigType struct {
-	Port     uint
-	Path     string
-	Resource []ResourceType
+	Port      uint
+	Path      string
+	Resources []ResourceType
 }
-
-type handlerFunc func(handler http.Handler) http.Handler
 
 // ResourceType is an endpoint that is accesible under Config.Path
 type ResourceType struct {
-	Name    string
-	Method  string
+	// Name of the Resource. This value has to escaped for special characters
+	Name string
+	// Only accepts on of the following: POST, GET, OPTIONS, CONNECT
+	Method string
+	// Handler function which will act on the incoming request
 	Handler handlerFunc
 }
 
@@ -27,16 +27,35 @@ type configError struct {
 	Message string
 }
 
+// FinalHandler is a helper http.HandlerFunc as the final closure to the Resource handler
+// Implementor can set his own final handler by config.FinalHandler = yourCustomFinalHandlerFunc
 var FinalHandler http.HandlerFunc
+
+// AllowedOrigins is an array of origins that are allowed if EnableCORS handler is used
+var AllowedOrigins []string
+
+// Local variables
+type handlerFunc func(handler http.Handler) http.Handler
+
+var gConfig ConfigType
 
 // StartServer starts a server with the specified config
 func StartServer(config ConfigType) error {
+
+	// store a global reference of the config
+	gConfig = config
+
 	// first thing... ensure that the config is valid
 	validateConfig(&config)
 
-	FinalHandler = http.HandlerFunc(final)
-	http.Handle(config.Path+"/"+config.Resource[0].Name, start(config.Resource[0].Handler(FinalHandler)))
-	http.Handle(config.Path+"/"+config.Resource[1].Name, start(config.Resource[1].Handler(FinalHandler)))
+	// the implementor has an option to set his own final handler.
+	if FinalHandler == nil {
+		FinalHandler = http.HandlerFunc(final)
+	}
+
+	for i := 0; i < len(config.Resources); i++ {
+		http.Handle(config.Path+"/"+config.Resources[i].Name, config.Resources[i].Handler(FinalHandler))
+	}
 	err := http.ListenAndServe(":"+fmt.Sprint(config.Port), nil)
 	if err != nil {
 		log.Panic(err)
@@ -45,12 +64,40 @@ func StartServer(config ConfigType) error {
 	return nil
 }
 
-func start(next http.Handler) http.Handler {
+// Pre-built Handlers that can be used.
+
+// EnableCORS enables Cross Origin Resource Sharing for a particular resource
+func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Executing startHandler")
+
+		// Check if it is a allowed Origin
+		if !isAllowedOrigin(r.Header.Get("Origin")) {
+			log.Printf("Origin %s not allowed", r.Header.Get("Origin"))
+			http.Error(w, "This origin is not authorised to access", 401)
+		}
+
+		// Get the method configured for this Resource
+		method, err := getMethodByResourceName(r.URL.EscapedPath())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("Setting CORS headers")
+		w.Header().Set("Access-Control-Allow-Methods", method)
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		w.Header().Set("Content-Type", "text/html")
+
+		// Stop here if its Preflighted OPTIONS request
+		if r.Method == "OPTIONS" {
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
+
+// Internal helper functions
 
 func final(w http.ResponseWriter, r *http.Request) {
 	log.Println("Executing finalHandler")
@@ -66,46 +113,48 @@ func validateConfig(config *ConfigType) error {
 		return &configError{"No valid PATH set."}
 	}
 
-	// if (*config).Resource.Name == "" {
-	// 	return &configError{"No valid RESOURCE set for path [" + (*config).Path + "]"}
-	// }
+	if len((*config).Resources) == 0 {
+		return nil
+	}
 
-	// if (*config).Resource.Method == "" {
-	// 	return &configError{"No valid METHOD set for resource [" + (*config).Resource.Name + "]"}
-	// }
+	for i := 0; i < len(config.Resources); i++ {
+		if (*config).Resources[i].Name == "" {
+			return &configError{"No valid RESOURCE set for path [" + (*config).Path + "]"}
+		}
 
-	// if (*config).Resource.Handler == nil {
-	// 	return &configError{"No valid HANDLER set for resource [" + (*config).Resource.Name + "]"}
-	// }
+		if (*config).Resources[i].Method == "" {
+			return &configError{"No valid METHOD set for resource [" + (*config).Resources[i].Name + "]"}
+		}
+
+		if (*config).Resources[i].Handler == nil {
+			return &configError{"No valid HANDLER set for resource [" + (*config).Resources[i].Name + "]"}
+		}
+	}
 
 	return nil
 }
 
-func enableCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Setting CORS headers")
-		w.Header().Set("Access-Control-Allow-Methods", "POST")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-		w.Header().Set("Content-Type", "text/html")
-
-		// Stop here if its Preflighted OPTIONS request
-		if r.Method == "OPTIONS" {
-			return
+func getMethodByResourceName(name string) (string, error) {
+	for _, a := range gConfig.Resources {
+		if a.Name == name {
+			return a.Method, nil
 		}
+	}
 
-		next.ServeHTTP(w, r)
-	})
+	return "", &configError{"Resource with name " + name + " not found."}
+}
+
+func isAllowedOrigin(origin string) bool {
+	for _, allowed := range AllowedOrigins {
+		if origin == allowed {
+			return true
+		}
+	}
+
+	// no valid origin found; so return false
+	return false
 }
 
 func (e *configError) Error() string {
 	return e.Message
-}
-
-func wvHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from WV, %q, %q", html.EscapeString(r.URL.Path), r.URL.Query())
-}
-
-func fpHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from FP, %q, %q", html.EscapeString(r.URL.Path), r.URL.Query())
 }
--- middleware_test.go
+++ middleware_test.go
@@ -7,27 +7,32 @@ import (
 	"testing"
 )
 
-func TestStartServer(t *testing.T) {
-	resource := []ResourceType{ResourceType{"name1", "GET", testHandler1(testHandler2(FinalHandler))}, ResourceType{"name2", "GET", testHandler2}}
+var config ConfigType
 
-	config := ConfigType{8080, "/fp", resource}
+func TestStartServer(t *testing.T) {
+	resource := []ResourceType{ResourceType{"name1", "GET", testHandler}, ResourceType{"name2", "GET", testHandler2}}
+
+	config = ConfigType{8080, "/fp", resource}
 	StartServer(config)
 }
 
-func testHandler1(handler http.Handler) http.Handler {
+func testHandler(handler http.Handler) http.Handler {
+	return testHandler1(testHandler2(FinalHandler))
+}
+
+func testHandler1(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Got a %s request for: %v", r.Method, r.URL)
-		w.Write([]byte(fmt.Sprintf("Got a %s request for: %v", r.Method, r.URL)))
+		w.Write([]byte(fmt.Sprintf("\nHandler 1 - Got a %s request for: %v", r.Method, r.URL)))
 		log.Println("Handler finished processing request")
-		return
+		next.ServeHTTP(w, r)
 	})
 }
 
-func testHandler2(handler http.Handler) http.Handler {
+func testHandler2(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Got a %s request for: %v", r.Method, r.URL)
-		w.Write([]byte(fmt.Sprintf("Got a %s request for: %v", r.Method, r.URL)))
+		w.Write([]byte(fmt.Sprintf("\nHandler 2 - Got a %s request for: %v", r.Method, r.URL)))
 		log.Println("Handler finished processing request")
-		handler.ServeHTTP(w, r)
 	})
 }
