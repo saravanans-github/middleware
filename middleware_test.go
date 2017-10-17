c0d946139958319e926cb900dff89b1975e23a94
Saravanan <saravanans.github@outlook.sg>
2017-10-17 17:33:34 +0800

    added test cases for new methods added in middleware.go

--- middleware_test.go
+++ middleware_test.go
@@ -1,38 +1,125 @@
 package middleware
 
 import (
-	"fmt"
+	"bytes"
 	"log"
 	"net/http"
+	"strings"
 	"testing"
 )
 
 var config ConfigType
 
-func TestStartServer(t *testing.T) {
-	resource := []ResourceType{ResourceType{"/name1", "GET", testHandler}, ResourceType{"/name2", "GET", testHandler2}}
+func initTest() {
+	log.SetFlags(0)
+}
 
-	config = ConfigType{8080, "/fp", resource}
+func TestStartStopServer(t *testing.T) {
+	initTest()
+
+	resource := []ResourceType{ResourceType{"/name1", "POST", testStartStopServerHandler(FinalHandler)}}
+
+	config = ConfigType{Port: 8080, Path: "/fp", Resources: resource}
 	StartServer(config)
+
+	if status := sendRequest("http://localhost:8080/fp/name1", []byte("Hello World")); status != 200 {
+		StopServer()
+		t.FailNow()
+	} else {
+		StopServer()
+	}
 }
 
-func testHandler(handler http.Handler) http.Handler {
-	return IsRequestValid(testHandler2(FinalHandler))
+func TestIsRequestValid(t *testing.T) {
+	initTest()
+
+	resource := []ResourceType{ResourceType{"/name1", "GET", IsRequestValid(testHandler2(FinalHandler))}}
+
+	config = ConfigType{Port: 8080, Path: "/fp", Resources: resource}
+	StartServer(config)
+	if status := sendRequest("http://localhost:8080/fp/name1", []byte("Hello World")); status != 400 {
+		StopServer()
+		t.FailNow()
+	} else {
+		StopServer()
+	}
 }
 
-func testHandler1(next http.Handler) http.Handler {
+func TestResourceNotFound(t *testing.T) {
+	resource := []ResourceType{ResourceType{"/name1", "POST", IsRequestValid(testHandler2(FinalHandler))}}
+
+	config = ConfigType{Port: 8080, Path: "/fp", Resources: resource}
+	StartServer(config)
+
+	if status := sendRequest("http://localhost:8080/fp/name2", []byte("Hello World")); status != 404 {
+		StopServer()
+		t.FailNow()
+	} else {
+		StopServer()
+	}
+}
+
+func TestValidateConfig_ForInvalidPath(t *testing.T) {
+	resource := []ResourceType{ResourceType{"/name1", "POST", IsRequestValid(testHandler2(FinalHandler))}}
+
+	config = ConfigType{Port: 8080, Path: "", Resources: resource}
+	if err := StartServer(config); !strings.HasPrefix(err.Error()[:], _ErrorInvalidPath) {
+		t.FailNow()
+	}
+
+	config = ConfigType{Port: 8080, Path: "fp", Resources: resource}
+	if err := StartServer(config); !strings.HasPrefix(err.Error()[:], _ErrorInvalidPath) {
+		t.FailNow()
+	}
+}
+
+func TestValidateConfig_ForInvalidPort(t *testing.T) {
+	resource := []ResourceType{ResourceType{"/name1", "POST", IsRequestValid(testHandler2(FinalHandler))}}
+
+	config = ConfigType{Port: 1000000, Path: "/fp", Resources: resource}
+	if err := StartServer(config); !strings.HasPrefix(err.Error()[:], _ErrorInvalidPort) {
+		t.FailNow()
+	}
+}
+
+func TestValidateConfig_ForEmptyResources(t *testing.T) {
+	resource := []ResourceType{}
+
+	config = ConfigType{Port: 8080, Path: "/fp", Resources: resource}
+	if err := StartServer(config); !strings.HasPrefix(err.Error()[:], _ErrorEmptyResources) {
+		t.FailNow()
+	}
+}
+
+func testStartStopServerHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Got a %s request for: %v", r.Method, r.URL)
-		w.Write([]byte(fmt.Sprintf("\nHandler 1 - Got a %s request for: %v", r.Method, r.URL)))
-		log.Println("Handler finished processing request")
+	})
+}
+
+func testIsRequestValidHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
 	})
 }
 
 func testHandler2(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Got a %s request for: %v", r.Method, r.URL)
-		w.Write([]byte(fmt.Sprintf("\nHandler 2 - Got a %s request for: %v", r.Method, r.URL)))
-		log.Println("Handler finished processing request")
+		next.ServeHTTP(w, r)
 	})
+}
+
+func testResourceNotFoundHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func sendRequest(url string, data []byte) int {
+
+	res, err := http.Post(url, "application/json", bytes.NewBuffer(data)) //http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	return res.StatusCode
 }
